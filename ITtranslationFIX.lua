b4e2a541579dbc504c22c03b3177feb26d861a9f
martibarimaff <97847284+martibarimaff@users.noreply.github.com>
2024-10-29 15:03:37 +0100

    Update ai fix e aggiunti nuovi

--- ITtranslationFIX.lua
+++ ITtranslationFIX.lua
@@ -11,10 +11,18 @@
 
 sendDebugMessage("Launching IT Translation FIX")
 
--- Fixes the "seeing double" description
--- Fixa la descrizione di "Visione doppia"
+
 SMODS.Joker:take_ownership('j_seeing_double', {
 	loc_txt = {
+		['en-us'] = {
+			name="Seeing Double",
+            text = {
+				"{X:mult,C:white} X#1# {} Mult if played",
+				"hand has a scoring",
+				"{C:clubs}Club{} card and a scoring",
+				"card of any other {C:attention}suit",
+			},
+		},
 		['it'] = {
 			name = "Visione doppia",
 			text = {
@@ -28,18 +36,149 @@ SMODS.Joker:take_ownership('j_seeing_double', {
 })
 
 
-
-SMODS.Tag:take_ownership('store_joker_create', {
+SMODS.Tag:take_ownership('tag_handy', {
 	loc_txt = {
+		['en-us'] = {
+			name="Handy Tag",
+                text={
+                    "Gives {C:money}$#1#{} per played",
+                    "{C:blue}hand{} this run",
+                    "{C:inactive}(Will give {C:money}$#2#{C:inactive})",
+                },
+		},
 		['it'] = {
-			name = "Patto non comune",
-			text = {
-				"Il negozio ha un {C:green}Jolly",
-				"non comune {C:attention}gratuito{}"
-                }
-            }
+			name="Patto maneggevole",
+                text={
+                    "Dà {C:money}$#1#{} per",
+                    "{C:blue}mano{} giocata in questa sessione",
+                    "{C:inactive}(Darà {C:money}$#2#{C:inactive})",
+                },
+		},		
 	}
 })
+
+SMODS.Tag:take_ownership('tag_rare', {
+	loc_txt = {
+		['en-us'] = {
+			name="Rare Tag",
+                text={
+                    "Shop has a free",
+                    "{C:red}Rare Joker",
+                },
+		},
+		['it'] = {
+			name="Patto raro",
+                text={
+                    "Il prossimo negozio ha un",
+                    "{C:red}Jolly raro{} gratuito",
+                },
+		},		
+	}
+})
+
+SMODS.Tag:take_ownership('tag_uncommon', {
+	loc_txt = {
+		['en-us'] = {
+			name="Uncommon Tag",
+                text={
+                    "Shop has a free",
+                    "{C:green}Uncommon Joker",
+                },
+		},
+		['it'] = {
+			name="Patto non comune",
+                text={
+                    "Il prossimo negozio ha un",
+                    "{C:green}Jolly non comune{} gratuito",
+                },
+		},		
+	}
+})
+
+SMODS.Tag:take_ownership('tag_polychrome', {
+	loc_txt = {
+		['en-us'] = {
+			name="Polychrome Tag",
+                text={
+                    "Next base edition shop",
+                    "Joker is free and",
+                    "becomes {C:dark_edition}Polychrome",
+                },
+		},
+		['it'] = {
+			name="Patto policromo",
+                text={
+                    "Il prossimo Jolly edizione base",
+					"nel negozio è gratuito e",
+                    "diventa {C:dark_edition}policromo",
+                },
+		},		
+	}
+})
+
+SMODS.Tag:take_ownership('tag_negative', {
+	loc_txt = {
+		['en-us'] = {
+			name="Negative Tag",
+                text={
+                    "Next base edition shop",
+                    "Joker is free and",
+                    "becomes {C:dark_edition}Negative",
+                },
+		},
+		['it'] = {
+			name="Patto negativo",
+                text={
+					"Il prossimo Jolly edizione base",
+					"nel negozio è gratuito e",
+                    "diventa {C:dark_edition}negativo",
+                },
+		},		
+	}
+})
+
+SMODS.Tag:take_ownership('tag_foil', {
+	loc_txt = {
+		['en-us'] = {
+			name="Foil Tag",
+                text={
+                    "Next base edition shop",
+                    "Joker is free and",
+                    "becomes {C:dark_edition}Foil",
+                },
+		},
+		['it'] = {
+			name="Patto foil",
+                text={
+                    "Il prossimo Jolly edizione base",
+					"nel negozio è gratuito e",
+                    "diventa {C:dark_edition}foil",
+                },
+		},		
+	}
+})
+
+SMODS.Tag:take_ownership('tag_holo', {
+	loc_txt = {
+		['en-us'] = {
+			name="Holographic Tag",
+                text={
+                    "Next base edition shop",
+                    "Joker is free and",
+                    "becomes {C:dark_edition}Holographic",
+                },
+		},
+		['it'] = {
+			name="Patto olografico",
+                text={
+                    "Il prossimo Jolly edizione base",
+					"nel negozio è gratuito e",
+                    "diventa {C:dark_edition}olografico",
+                },
+		},		
+	}
+})
+
 
 
 ----------------------------------------------
