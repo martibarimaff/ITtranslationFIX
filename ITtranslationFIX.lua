6dc769c38e1c3ea87742bc8f39513f890192f813
martibarimaff <97847284+martibarimaff@users.noreply.github.com>
2025-02-02 04:23:52 +0100

    refactoring metadata

--- ITtranslationFIX.lua
+++ ITtranslationFIX.lua
@@ -1,10 +1,3 @@
---- STEAMODDED HEADER
---- MOD_NAME: IT Translation FIX
---- MOD_ID: ITtranslationFIX
---- MOD_AUTHOR: [martibarimaff]
---- MOD_DESCRIPTION: fixes the italian tranlation of some things, and does hopefully nothing else
---- LOADER_VERSION_GEQ: 1.0.0
-
 ----------------------------------------------
 ------------MOD CODE -------------------------
 
