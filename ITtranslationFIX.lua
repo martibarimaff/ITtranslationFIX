019666295367ea79fe3b88675856bd9ec36cbf95
martibarimaff <97847284+martibarimaff@users.noreply.github.com>
2025-02-14 22:01:49 +0100

    Added in-game link to the repo issues page by clicking translation feedback in the language menu

--- ITtranslationFIX.lua
+++ ITtranslationFIX.lua
@@ -6,5 +6,14 @@ sendDebugMessage("Launching IT Translation FIX")
 
 SMODS.Language:take_ownership('it',{})
 
+
+G.FUNCS.loc_survey = function(e)
+	if  G.SETTINGS.language == 'it' then
+	love.system.openURL( "https://github.com/martibarimaff/ITtranslationFIX/issues" )
+	else
+	love.system.openURL( "https://forms.gle/pL5tMh1oXLmv8czz9" )
+	end
+end
+
 ----------------------------------------------
 ------------MOD CODE END----------------------
